5d120d5931afea7200ffa163278bbf3c76205477
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-03-10 11:04:29 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,44 +1,52 @@
-bms_attribute_limits = {'TEMPERATURE': {'min': 0, 'max': 45},
-                        'SOC': {'min': 20, 'max': 80},
-                        'CHARGE_RATE': {'min': 0,'max': 0.8}} 
-                        
-def report_battery_vitals(bms_attributes):
-    abnormal_vitals = filter(filterOut_safe_vitals,bms_attributes.items())
-    if len(abnormal_vitals) > 0:
-        report_abnormal_vitals(abnormal_vitals,bms_attribute_limits)
+Battery_limits = {'TEMPERATURE': {'min': 0, 'max': 45, 'warn': 5 },
+                   'SOC': {'min': 20, 'max': 80, 'warn': 5 },
+                   'CHARGE_RATE': {'min': 0,'max': 0.8, 'warn': 5 }} 
+
+lang_DE ={'warning' : "Warnung! Bitte halten Sie innerhalb des Bereichs",
+	  'Okay' : "Batterie ist in einwandfreiem Zustand",
+	  'fail' : "Batterielimits A-Range!"}
+
+lang_EN = {'warning' : "Warning! Please maintain wihtin Range",
+	   'Okay' : "Battery in Perfect Condition",
+	   'fail' : " Battery Limits out of Range!"}
+lang = "EN"
+
+def filter_values(item):
+    low = Battery_limits[item[0].upper()]['min']
+    high = Battery_limits[item[0].upper()]['max']    
+    return (item[1] < low) or (item[1] > high)
+
+def battery_is_ok(battery_attributes):
+   wrong_val = filter(filter_values,battery_attributes.items())
+   if len(wrong_val) > 0:
+        Message(wrong_val)
     else:
-        report_normal_vitals()
+        if lang ="EN":
+		print(lang_EN[item[1])
+        else lang = 0:
+                print(lang_DE[item[1])
         
-def report_normal_vitals():
-    print("Battery is Ok")
-    
-def report_abnormal_vitals(abnormal_vitals,vital_limits):
-    print("Abnormal Vitals")    
-    for i in abnormal_vitals.items():
-        low_breach_val = vital_limits[i[0].upper()]['min']
-        high_breach_val = vital_limits[i[0].upper()]['max']
-        if i[1] < low_breach_val:
-            print("Low Breach : "+i[0].upper()+" | Current value: "+str(i[1]) +" | Lower limit value: "+str(low_breach_val))
-        elif i[1] > high_breach_val:
-            print("High Breach : "+i[0].upper()+" | Current Value: "+str(i[1])+" | Upper limit value: "+str(high_breach_val))
-            
-
-
-def filterOut_safe_vitals(item):
-    low_breach_val = bms_attribute_limits[item[0].upper()]['min']
-    high_breach_val = bms_attribute_limits[item[0].upper()]['max']    
-    return (item[1] < low_breach_val) or (item[1] > high_breach_val)
-	
-def is_battery_ok(bms_attributes):
-    value = dict(filter(filterOut_safe_vitals,bms_attributes.items()))
-    report_battery_state(bms_attributes)
-    return len(value) == 0
-
-def report_battery_state(bms_attributes):    
-    report_battery_vitals(bms_attributes)
-
-    
+def Message(wrong_val):
+    for i in wrong_val.items():
+        low = Battery_limits[i[0].upper()]['min']
+        high = Battery_limits[i[0].upper()]['max']
+	low_warning = low + ( Battery_limits[i[1].upper()]['max'] * Battery_limits[i[2].upper()]['warn'])/100
+    	high_warning = high * (100 - Battery_limits[i[2].upper()]['warn'])/100
+	if low_warning>= high_warning :
+		if lang = "EN" :
+			print(lang_EN[item[0])
+		elif lang = "DE":
+			print(lang_DE[item[0])
+        elif i[1] <= low_breach_val or i[1] >= high_breach_val:
+		if lang = "EN" :
+			print(lang_EN[item[2])
+		elif lang = "DE":
+			print(lang_DE[item[2])
+	                              
+                
 if __name__ == '__main__':
-  assert(is_battery_ok({'temperature': 25,'Soc': 70, 'Charge_rate': 0.7}) is True)  #all attributes fine
-  assert(is_battery_ok({'Temperature': 50,'soc': 85, 'Charge_rate': 0}) is False)  #all attributes off limit
-  assert(is_battery_ok({'Temperature': 100,'SOC': 30, 'Charge_rate': 0.5}) is False) #temp off limit
+        battery_is_ok({'temperature': 25,'soc': 70, 'roc': 0.7})
+        battery_is_ok({'temperature': 50,'soc': 85, 'roc': 0})
+	language = "DE"
+    	battery_is_ok({'Temperature': 100,'SOC': 30, 'Charge_rate': 0.5}) 
+    			      
