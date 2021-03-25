da98fc88f46d1f8fa8c0f4120e8f8f4faaae931f
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-03-25 18:39:08 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,4 +1,6 @@
-battery_limits = {'Temperature': {'min' : 0, 'max' : 45}, 'SOC': {'min' : 20, 'max' : 80}, 'Rate_of_Charge': {'min' : 0, 'max' : 0.8}}
+battery_limits = {'Temperature': {'min' : 0, 'max' : 45, 'de' : "Temperatur"}, 
+		  'SOC': {'min' : 20, 'max' : 80, 'de' : "SOC"}, 
+		  'Rate_of_Charge': {'min' : 0, 'max' : 0.8, 'de' : "Rate_der_Gebühr"}}
 
 Msg_Info = {'low_breach': {'DE' : "Nicht! Wert ist zu niedrig", 'EN' : "Failing! value is too low"}, 
 	    'low_warning' : {'DE' : "Warnung! Wert ist zu niedrig", 'EN' : "Warning! value is too Low"},
@@ -9,20 +11,17 @@ Msg_Info = {'low_breach': {'DE' : "Nicht! Wert ist zu niedrig", 'EN' : "Failing!
 Controller_Msg_EN = ["", "", "Battery in Stable Condition. Maintain It", "", ""]
 Controller_Msg_DE = ["","" ,"Batterie in Stabilem Zustand. Maintain It", "", ""]
 
-factor_en = ["Temperature","SOC", "Rate_of_Charge"]
-factor_de = ["Temperatur","SOC", "Rate_der_Gebühr"]
-
 lang = "EN"
 
 def Threshold(max):
 	warn_limit = 5
 	return (max * warn_limit) / 100
 
-def Display_Msg_on_Console(num, value, attribute):
+def Display_Msg_on_Console(battery_parameter, battery_limit_values, value, attribute):
 	if (lang == "EN"):
-		return print(Msg_Info[attribute][lang], factor_en[num], ":", value)
+		return print(Msg_Info[attribute][lang], battery_parameter, ":", value)
 	else:
-		return print(Msg_Info[attribute][lang], factor_de[num], ":", value)
+		return print(Msg_Info[attribute][lang], battery_limit_value['de'], ":", value)
 
 def Controller_Info(val):
 	if (lang == "EN"):
@@ -30,30 +29,30 @@ def Controller_Info(val):
 	else:
 		return Controller_Msg_DE[val]
 
-def Check_Warning_Range(value, min_limit, max_limit):
+def Check_Warning_Range(battery_parameter, battery_limit_values, value, min_limit, max_limit):
 	low_warning = min_limit + Threshold(max_limit)
 	high_warning = max_limit - Threshold(max_limit)
 	if value >= min_limit and value <= low_warning:
-		Display_Msg_on_Console(index_value, value, "low_warning")
+		Display_Msg_on_Console(battery_parameter, battery_limit_values, value, "low_warning")
 	elif value >= high_warning and value <= max_limit:
-		Display_Msg_on_Console(index_value, value, "high_warning")	
+		Display_Msg_on_Console(battery_parameter, battery_limit_values, value, "high_warning")	
 	else:
-		Display_Msg_on_Console(index_value, value, "Okay")
+		Display_Msg_on_Console(battery_parameter, battery_limit_values, value, "Okay")
 
-def Check_Off_Limit_Range(index_value, value, battery_limit_values):
+def Check_Off_Limit_Range(battery_parameter, value, battery_limit_values):
 	min_limit = battery_limit_values['min']
 	max_limit = battery_limit_values['max']
 	if value < min_limit:
-		Display_Msg_on_Console(index_value, value, 'low_breach')
+		Display_Msg_on_Console(battery_limit_values, value, 'low_breach')
 	elif value > max_Limit:
-		Display_Msg_on_Console(index_value, value, 'high_breach')
+		Display_Msg_on_Console(battery_limit_values, value, 'high_breach')
 	else:
-		Check_Warning_Range(value, min_limit, max_limit)
+		Check_Warning_Range(battery_parameter, battery_limit_values, value, min_limit, max_limit)
 		
 def Is_Battery_OK(Battery_Value_Parameters):
 	print("Here is the result for the input values:")
-	for battery_parameter, index_value in enumerate(Battery_Value_Parameters):
-		Check_Off_Limit_Range(index_value, Battery_Value_Parameters[battery_parameter], battery_limits[battery_parameter])
+	for battery_parameter in Battery_Value_Parameters:
+		Check_Off_Limit_Range(battery_parameter, Battery_Value_Parameters[battery_parameter], battery_limits[battery_parameter])
 	print("\n")
 
 if __name__ == '__main__':
