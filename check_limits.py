533b7b961ed97421b41c6b62d04958af61e0562d
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-03-25 18:04:12 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,58 +1,64 @@
 battery_limits = {'Temperature': {'min' : 0, 'max' : 45}, 'SOC': {'min' : 20, 'max' : 80}, 'Rate_of_Charge': {'min' : 0, 'max' : 0.8}}
-					
-def Threshold(max):
-	warn_limit = 5
-	return (max * warn_limit) / 100
 
 Msg_Info = {'low_breach': {'DE' : "Nicht! Wert ist zu niedrig", 'EN' : "Failing! value is too low"}, 
 	    'low_warning' : {'DE' : "Warnung! Wert ist zu niedrig", 'EN' : "Warning! value is too Low"},
-	    'Okay' : {'DE' : "Wert ist perfekt, Halten Sie es", 'EN' : "Value is perfect, Maintain it"},
+	    'Okay' : {'DE' : "Wert ist perfekt", 'EN' : "Value is perfect"},
 	    'high_warning' : {'DE' : "Warnung! Wert ist zu hoch", 'EN' : "Warning! value is too High"},
 	    'high_breach' : {'DE' : "Nicht! Wert ist zu hoch", 'EN' : "Failing! value is too High"}}
 
-lang_msg ={'DE' : {'warning' : "Warnung! Wert hat Schwellenwert erreicht" , 'Okay' : "Batterie ist in einwandfreiem Zustand", 'fail' : "Batterielimits A-Range!"},
-	   'EN' : {'warning' : "Warning! Value has reached threshold limit" , 'Okay' : "Battery in Perfect Condition", 'fail' : " Battery Limits out of Range!"}}
-
-Controller_Msg_EN = ["", "", "", "", ""]
-Controller_Msg_DE
+Controller_Msg_EN = ["", "", "Battery in Stable Condition. Maintain It", "", ""]
+Controller_Msg_DE = [ ,"Batterie in Stabilem Zustand. Maintain It", ]
 
 factor_en = ["Temperature","SOC", "Rate_of_Charge"]
 factor_de = ["Temperatur","SOC", "Rate_der_Gebühr"]
 
 lang = "EN"
 
-def Display_Msg_on_Console(value, num, attribute):
-	return print(lang_msg[lang][attribute], factor_de[num], ":", value)
+def Threshold(max):
+	warn_limit = 5
+	return (max * warn_limit) / 100
 
-def Is_Battery_in_good_condition(condition):
-	if(condition == 1):
-		print(lang_msg[lang]['Okay'])
-		
-def Check_range(value, Num):
-	low = battery_limits[factor_en[Num]]['min']
-	high = battery_limits[factor_en[Num]]['max']
-	warning_value = threshold_val(high)
-	low_warning = low + warning_value
-	high_warning = high - warning_value
-	if value <= low or value >= high:
-		Display_Msg(value, Num, "fail")
-		return 0
-	compare_battery_param_value(low_warning, value, Num, high_warning)
-	return 1
+def Display_Msg_on_Console(num, value, attribute):
+	if (lang == "EN"):
+		return print(Msg_Info[attribute][lang], factor_en[num], ":", value)
+	else:
+		return print(Msg_Info[attribute][lang], factor_de[num], ":", value)
 
-def compare_battery_param_value(lower_value, value, Num, upper_value):
-	if lower_value >= value or value >= upper_value:
-		Display_Msg(value, Num, "warning")
-		return 0
+def Controller_Info(val):
+	if (lang == "EN"):
+		return Controller_Msg_EN[val]
+	else:
+		return Controller_Msg_DE[val]
+
+def Check_Warning_Range(value, min_limit, max_limit):
+	low_warning = min_limit + Threshold(max_limit)
+	high_warning = max_limit - Threshold(max_limit)
+	if value >= min_limit and value <= low_warning:
+		Display_Msg_on_Console(index_value, value, "low_warning")
+	elif value >= high_warning and value <= max_limit:
+		Display_Msg_on_Console(index_value, value, "high_warning")	
+	else:
+		Display_Msg_on_Console(index_value, value, "Okay")
+
+def Check_Off_Limit_Range(index_value, value, battery_limit_values):
+	min_limit = battery_limit_values['min']
+	max_limit = battery_limit_values['max']
+	if value < min_limit:
+		Display_Msg_on_Console(index_value, value, 'low_breach')
+	elif value > max_Limit:
+		Display_Msg_on_Console(index_value, value, 'high_breach')
+	else:
+		Check_Warning_Range(value, min_limit, max_limit)
 		
-def battery_is_ok(temperature, soc, roc):
-	status =(Check_range(temperature, 0)) & (Check_range(soc, 1)) & (Check_range(roc, 2))
-	Is_Battery_in_good_condition(status)
-	return (status)
-	
+def Is_Battery_OK(Battery_Value_Parameters):
+	print("Here is the result for the input values:")
+	for battery_parameter, index_value in enumerate(Battery_Value_Parameters):
+		Check_Off_Limit_Range(index_value, Battery_Value_Parameters[battery_parameter], battery_limits[battery_parameter])
+	print("\n")
+
 if __name__ == '__main__':
-	battery_is_ok(25, 77, 0)
-	battery_is_ok(50, 23, 0.8)
+	Is_Battery_OK({'Temperature' : 25, 'SOC' : 77, 'Rate_Of_Charge' : 0})
+	Is_Battery_OK({'Temperature' : 50, 'SOC' : 23, 'Rate_Of_Charge' : 0.8})
 	lang = "DE"
-	battery_is_ok(25, 70, 0.7)
-	battery_is_ok(50, 85,  0)
+	Is_Battery_OK({'Temperature' : 25, 'SOC' : 70, 'Rate_Of_Charge' : 0.7})
+	Is_Battery_OK({'Temperature' : 50, 'SOC' : 85, 'Rate_Of_Charge' : 0})
