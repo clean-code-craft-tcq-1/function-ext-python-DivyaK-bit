44d844fa7bde21a49a35df50cf642c806bbca8e4
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-03-26 14:59:27 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,40 +1,15 @@
-from Controller_Message import *
-from Console_message import *
+from Compare_Battery_Range import *
 
 battery_limits = {'Temperature': {'min' : 0, 'max' : 45, 'de' : "Temperatur"}, 
 		  'SOC': {'min' : 20, 'max' : 80, 'de' : "SOC"}, 
 		  'Rate_of_Charge': {'min' : 0, 'max' : 0.8, 'de' : "Rate_der_Gebühr"}}
 
 lang = "EN"
-
-def Threshold(max):
-	warn_limit = 5
-	return (max * warn_limit) / 100
-
-def Check_Warning_Range(battery_parameter, battery_limit_values, value, min_limit, max_limit):
-	low_warning = min_limit + Threshold(max_limit)
-	high_warning = max_limit - Threshold(max_limit)
-	if value >= min_limit and value <= low_warning:
-		Display_Msg_on_Console(battery_parameter, battery_limit_values, value, "low_warning", lang)
-	elif value >= high_warning and value <= max_limit:
-		Display_Msg_on_Console(battery_parameter, battery_limit_values, value, "high_warning", lang)	
-	else:
-		Display_Msg_on_Console(battery_parameter, battery_limit_values, value, "Okay", lang)
-
-def Check_Off_Limit_Range(battery_parameter, value, battery_limit_values):
-	min_limit = battery_limit_values['min']
-	max_limit = battery_limit_values['max']
-	if value < min_limit:
-		Display_Msg_on_Console(battery_parameter, battery_limit_values, value, 'low_breach', lang)
-	elif value > max_limit:
-		Display_Msg_on_Console(battery_parameter, battery_limit_values, value, 'high_breach',lang)
-	else:
-		Check_Warning_Range(battery_parameter, battery_limit_values, value, min_limit, max_limit)
 		
 def Is_Battery_OK(Battery_Value_Parameters):
 	print("Here is the result for the input values:")
 	for battery_parameter in Battery_Value_Parameters:
-		Check_Off_Limit_Range(battery_parameter, Battery_Value_Parameters[battery_parameter], battery_limits[battery_parameter])
+		Check_Off_Limit_Range(battery_parameter, Battery_Value_Parameters[battery_parameter], battery_limits[battery_parameter], lang)
 	print("\n")
 
 if __name__ == '__main__':
