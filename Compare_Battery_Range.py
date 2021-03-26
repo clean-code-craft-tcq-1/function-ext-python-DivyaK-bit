626383bbb62a3ac27e66aa006d27ab729ebc3d34
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-03-26 15:09:23 +0530

    Update Compare_Battery_Range.py

--- Compare_Battery_Range.py
+++ Compare_Battery_Range.py
@@ -1,16 +1,5 @@
 from Controller_Message import *
 from Console_message import *
-from check_limits import *
-
-def Check_Warning_Range(battery_parameter, battery_limit_values, value, min_limit, max_limit, lang):
-	low_warning = min_limit + Threshold(max_limit)
-	high_warning = max_limit - Threshold(max_limit)
-	if value >= min_limit and value <= low_warning:
-		Display_Msg_on_Console(battery_parameter, battery_limit_values, value, "low_warning", lang)
-	elif value >= high_warning and value <= max_limit:
-		Display_Msg_on_Console(battery_parameter, battery_limit_values, value, "high_warning", lang)	
-	else:
-		Display_Msg_on_Console(battery_parameter, battery_limit_values, value, "Okay", lang)
 
 def Check_Off_Limit_Range(battery_parameter, value, battery_limit_values, lang):
 	min_limit = battery_limit_values['min']
