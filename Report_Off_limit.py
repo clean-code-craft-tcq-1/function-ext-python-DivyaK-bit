22b188f6a1d555a8f07190ed3c72fb5ce39627ef
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-03-29 18:25:09 +0530

    Update Report_Off_limit.py

--- Report_Off_limit.py
+++ Report_Off_limit.py
@@ -7,7 +7,9 @@ def Check_Off_Limit_Range(battery_parameter, value, battery_limit_values, lang):
 	max_limit = battery_limit_values['max']
 	if value < min_limit:
 		Display_Msg_on_Console(battery_parameter, value, 'low_breach', lang)
+		Controller_Info(battery_parameter, 0, lang)
 	elif value > max_limit:
 		Display_Msg_on_Console(battery_parameter, value, 'high_breach',lang)
+		Controller_Info(battery_parameter, 4, lang)
 	else:
 		Check_Warning_Range(battery_parameter, value, min_limit, max_limit, lang)
