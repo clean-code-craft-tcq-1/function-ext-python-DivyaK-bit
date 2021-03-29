18b19a5f829592c318a9a49c36f3540cae31f2ac
DivyaK-bit <77484258+DivyaK-bit@users.noreply.github.com>
2021-03-29 15:55:21 +0530

    Update Report_Warning_limit.py

--- Report_Warning_limit.py
+++ Report_Warning_limit.py
@@ -2,12 +2,16 @@
 from Controller_Message import *
 from Console_message import *
 
-def Check_Warning_Range(battery_parameter, value, min_limit, max_limit, lang):
+def Threshold_Values(min_limit, max_limit):
 	low_warning = min_limit + (max_limit * 0.05)
 	high_warning = max_limit - (max_limit * 0.05)
-	if min_limit <= value <= low_warning and round(value, 2) == value:
+	return low_warning, high_warning
+
+def Check_Warning_Range(battery_parameter, value, min_limit, max_limit, lang):
+	low_warning, high_warning = Threshold_Values(min_limit, max_limit)
+	if min_limit <= value <= low_warning:
 		Display_Msg_on_Console(battery_parameter, value, "low_warning", lang)
-	elif high_warning <= value <= max_limit and round(value, 2) == value:
+	elif high_warning <= value <= max_limit:
 		Display_Msg_on_Console(battery_parameter, value, "high_warning", lang)	
 	else:
 		Display_Msg_on_Console(battery_parameter, value, "Okay", lang)
